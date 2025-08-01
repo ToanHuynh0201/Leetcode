d17ad5d7a2b76d645ba35b8faab86aac0de7fbca
ToanHuynh0201 <toanhuynhlilyiu@gmail.com>
2025-08-01 09:43:07 +0700

    118 119

---
+++ Day06/119-Pascal's-Triangle-II.ts
@@ -0,0 +1,11 @@
+function getRow(rowIndex: number): number[] {
+	const res: number[][] = [];
+	for (let i = 0; i < rowIndex + 1; i++) {
+		const row: number[] = new Array(i + 1).fill(1);
+		for (let j = 1; j < i; j++) {
+			row[j] = res[i - 1][j - 1] + res[i - 1][j];
+		}
+		res.push(row);
+	}
+	return res.pop()!;
+}
