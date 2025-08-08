f7859b573c245358135b23d28303726b142e351c
ToanHuynh0201 <toanhuynhlilyiu@gmail.com>
2025-08-08 07:36:35 +0700

    933

---
+++ Day11/933-Number-of-Recent-Calls.ts
@@ -0,0 +1,21 @@
+class RecentCounter {
+	private requests: number[];
+	constructor() {
+		this.requests = [];
+	}
+
+	ping(t: number): number {
+		this.requests.push(t);
+		while (this.requests[0] < t - 3000) {
+			this.requests.shift();
+		}
+
+		return this.requests.length;
+	}
+}
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * var obj = new RecentCounter()
+ * var param_1 = obj.ping(t)
+ */
