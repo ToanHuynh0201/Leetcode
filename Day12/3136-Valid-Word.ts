c6922157484f9ca08e9f32b8209c27b62deebae3
ToanHuynh0201 <toanhuynhlilyiu@gmail.com>
2025-08-09 11:13:33 +0700

    1957, 3136

---
+++ Day12/3136-Valid-Word.ts
@@ -0,0 +1,54 @@
+// function isValid(word: string): boolean {
+// 	if (word.length < 3) return false;
+
+// 	let vowels: number = 0;
+// 	let consonants: number = 0;
+
+// 	const vowelSet = new Set(["a", "e", "i", "o", "u"]);
+
+// 	for (const char of word) {
+// 		if (/\d/.test(char)) continue;
+
+// 		if (/[a-zA-Z]/.test(char)) {
+// 			if (vowelSet.has(char.toLowerCase())) {
+// 				vowels++;
+// 			} else {
+// 				consonants++;
+// 			}
+// 		} else {
+// 			return false;
+// 		}
+// 	}
+
+// 	return vowels > 0 && consonants > 0;
+// }
+
+function isValid(word: string): boolean {
+	if (word.length < 3) return false;
+
+	let hasVowel: boolean = false;
+	let hasConsonant: boolean = false;
+
+	const vowelSet = new Set(["a", "e", "i", "o", "u"]);
+
+	for (let i = 0; i < word.length; i++) {
+		if (!/^[a-zA-Z0-9]+$/.test(word.charAt(i))) {
+			return false;
+		}
+
+		if (/\d/.test(word.charAt(i))) continue;
+
+		if (!hasVowel && vowelSet.has(word.charAt(i).toLowerCase())) {
+			hasVowel = true;
+		}
+
+		if (
+			!hasConsonant &&
+			!vowelSet.has(word.charAt(i).toLocaleLowerCase())
+		) {
+			hasConsonant = true;
+		}
+	}
+
+	return hasConsonant && hasVowel;
+}
