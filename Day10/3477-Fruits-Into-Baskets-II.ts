5a990a8b41b8c53ba80a9529ba88bc6398d3b7a4
ToanHuynh0201 <toanhuynhlilyiu@gmail.com>
2025-08-11 15:51:26 +0700

    new 3330, preview 118, 3477, 231

--- Day10/3477-Fruits-Into-Baskets-II.ts
+++ Day10/3477-Fruits-Into-Baskets-II.ts
@@ -1,17 +1,34 @@
+// function numOfUnplacedFruits(fruits: number[], baskets: number[]): number {
+// 	let res = 0;
+// 	const n = baskets.length;
+// 	for (const fruit of fruits) {
+// 		let unpack = 1;
+// 		for (let i = 0; i < n; i++) {
+// 			if (fruit <= baskets[i]) {
+// 				unpack = 0;
+// 				baskets[i] = 0;
+// 				break;
+// 			}
+// 		}
+
+// 		res += unpack;
+// 	}
+// 	return res;
+// }
 function numOfUnplacedFruits(fruits: number[], baskets: number[]): number {
 	let res = 0;
-	const n = baskets.length;
+	const length = baskets.length;
 	for (const fruit of fruits) {
 		let unpack = 1;
-		for (let i = 0; i < n; i++) {
+		for (let i = 0; i < length; i++)
 			if (fruit <= baskets[i]) {
 				unpack = 0;
 				baskets[i] = 0;
 				break;
 			}
-		}
 
 		res += unpack;
 	}
+
 	return res;
 }
---
+++ Day13/3330-Find-the-Original-Typed-String-I.ts
@@ -0,0 +1,29 @@
+// function possibleStringCount(word: string): number {
+// 	let res = 1;
+// 	let last = word[word.length - 1];
+
+// 	for (let i = word.length - 2; i >= 0; i--) {
+// 		if (word[i] === last) {
+// 			word.slice(0, -1);
+// 			res++;
+// 		} else if (word[i] !== last) {
+// 			last = word[i];
+// 		}
+// 	}
+
+// 	return res;
+// }
+function possibleStringCount(word: string): number {
+	const regex = /([a-zA-Z])\1+/g;
+	if (!word.match(regex)) return 1;
+
+	const matches = word.match(regex);
+
+	return (
+		matches!.reduce(
+			(accumulator, currentValue) => accumulator + currentValue.length,
+			0
+		) -
+		(matches!.length - 1)
+	);
+}
