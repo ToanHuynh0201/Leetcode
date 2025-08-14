7500ae25b4e58947a51d75597a7e5d108086d65f
ToanHuynh0201 <toanhuynhlilyiu@gmail.com>
2025-08-14 10:26:12 +0700

    review 3330

--- Day13/3330-Find-the-Original-Typed-String-I.ts
+++ Day13/3330-Find-the-Original-Typed-String-I.ts
@@ -13,17 +13,31 @@
 
 // 	return res;
 // }
+
 function possibleStringCount(word: string): number {
-	const regex = /([a-zA-Z])\1+/g;
-	if (!word.match(regex)) return 1;
+	let res = 1;
+	let last = word[word.length - 1];
 
-	const matches = word.match(regex);
+	for (let i = word.length - 2; i >= 0; i--) {
+		if (word[i] === last) {
+			word.slice(0, -1);
+			res++;
+		} else if (word[i] !== last) last = word[i];
+	}
 
-	return (
-		matches!.reduce(
-			(accumulator, currentValue) => accumulator + currentValue.length,
-			0
-		) -
-		(matches!.length - 1)
-	);
+	return res;
 }
+// function possibleStringCount(word: string): number {
+// 	const regex = /([a-zA-Z])\1+/g;
+// 	if (!word.match(regex)) return 1;
+
+// 	const matches = word.match(regex);
+
+// 	return (
+// 		matches!.reduce(
+// 			(accumulator, currentValue) => accumulator + currentValue.length,
+// 			0
+// 		) -
+// 		(matches!.length - 1)
+// 	);
+// }
