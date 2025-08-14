112822b0bb7f97b2b2f29fafc5f58bb808d41d02
ToanHuynh0201 <toanhuynhlilyiu@gmail.com>
2025-08-14 10:22:15 +0700

    3304

--- Day13/3304-Find-the-K-th-Character-in-String-Game-I.ts
+++ Day13/3304-Find-the-K-th-Character-in-String-Game-I.ts
@@ -13,23 +13,37 @@
 // 	}
 // 	return word[k - 1];
 // }
+
 function kthCharacter(k: number): string {
-	const getNextChar = (char: string): string =>
-		char === "z" ? "a" : String.fromCharCode(char.charCodeAt(0) + 1);
-
-	const find = (k: number): string => {
-		if (k === 1) return "a";
-
-		let length = 1;
-		while (length < k) {
-			length *= 2;
+	let word = "a";
+	while (word.length < k) {
+		let nextPart = "";
+		for (const char of word) {
+			if (char === "z") nextPart += "a";
+			else nextPart += String.fromCharCode(char.charCodeAt(0) + 1);
 		}
+		word += nextPart;
+	}
 
-		const prevK = k <= length / 2 ? k : k - length / 2;
-		const char = find(prevK);
-
-		return k <= length / 2 ? char : getNextChar(char);
-	};
-
-	return find(k);
+	return word[k - 1];
 }
+// function kthCharacter(k: number): string {
+// 	const getNextChar = (char: string): string =>
+// 		char === "z" ? "a" : String.fromCharCode(char.charCodeAt(0) + 1);
+
+// 	const find = (k: number): string => {
+// 		if (k === 1) return "a";
+
+// 		let length = 1;
+// 		while (length < k) {
+// 			length *= 2;
+// 		}
+
+// 		const prevK = k <= length / 2 ? k : k - length / 2;
+// 		const char = find(prevK);
+
+// 		return k <= length / 2 ? char : getNextChar(char);
+// 	};
+
+// 	return find(k);
+// }
