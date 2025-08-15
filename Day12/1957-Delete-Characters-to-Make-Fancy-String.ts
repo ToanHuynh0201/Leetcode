4c9476bf0a908f6a9e50b522754ad59763dbf4da
ToanHuynh0201 <toanhuynhlilyiu@gmail.com>
2025-08-15 09:07:23 +0700

    review 1957

--- Day12/1957-Delete-Characters-to-Make-Fancy-String.ts
+++ Day12/1957-Delete-Characters-to-Make-Fancy-String.ts
@@ -1,6 +1,25 @@
+// function makeFancyString(s: string): string {
+// 	if (s.length < 3) return s;
+
+// 	let res: string = s[0];
+// 	let prev: string = s[0];
+// 	let flag = false;
+
+// 	for (let i = 1; i < s.length; i++) {
+// 		const c = s[i];
+// 		if (c !== prev) {
+// 			res += c;
+// 			prev = c;
+// 			flag = false;
+// 		} else if (!flag) {
+// 			res += c;
+// 			flag = true;
+// 		}
+// 	}
+// 	return res;
+// }
 function makeFancyString(s: string): string {
 	if (s.length < 3) return s;
-
 	let res: string = s[0];
 	let prev: string = s[0];
 	let flag = false;
