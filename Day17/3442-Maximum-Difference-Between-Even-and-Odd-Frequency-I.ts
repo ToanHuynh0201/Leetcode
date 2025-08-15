ab3a7f57b05e57ff59679c28643a37dc5e55ab28
ToanHuynh0201 <toanhuynhlilyiu@gmail.com>
2025-08-15 10:04:15 +0700

    3442

---
+++ Day17/3442-Maximum-Difference-Between-Even-and-Odd-Frequency-I.ts
@@ -0,0 +1,11 @@
+function maxDifference(s: string): number {
+	const map = new Map<string, number>();
+	for (const num of s) map.set(num, (map.get(num) ?? 0) + 1);
+
+	const values = Array.from(map.values());
+
+	const maxOdd = Math.max(...values.filter((v) => v % 2 !== 0));
+	const minEven = Math.min(...values.filter((v) => v % 2 === 0));
+
+	return maxOdd - minEven;
+}
