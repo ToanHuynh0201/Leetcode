face1fae809d72407d8a9d829d3c8ed9b3da8d03
ToanHuynh0201 <toanhuynhlilyiu@gmail.com>
2025-08-16 15:46:49 +0700

    2942

--- Day18/2942-Find-Words-Containing-Character.ts
+++ Day18/2942-Find-Words-Containing-Character.ts
@@ -1,1 +1,19 @@
-function findWordsContaining(words: string[], x: string): number[] {}
+// function findWordsContaining(words: string[], x: string): number[] {
+// 	const res: number[] = [];
+
+// 	for (let i = 0; i < words.length; i++) {
+// 		if (words[i].includes(x)) {
+// 			res.push(i);
+// 		}
+// 	}
+// 	return res;
+// }
+function findWordsContaining(words: string[], x: string): number[] {
+	const indices: number[] = [];
+
+	words.forEach((item, index) => {
+		if (item.includes(x)) indices.push(index);
+	});
+
+	return indices;
+}
