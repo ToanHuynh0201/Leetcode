e3c5877132fd4e94dcd022b01a6b1b219b17d854
ToanHuynh0201 <toanhuynhlilyiu@gmail.com>
2025-08-17 11:16:51 +0700

    review 2138, 2016, 3442

--- Day14/2138-Divide-a-String-Into-Groups-of-Size-k.ts
+++ Day14/2138-Divide-a-String-Into-Groups-of-Size-k.ts
@@ -16,19 +16,19 @@
 // 	return res;
 // }
 function divideString(s: string, k: number, fill: string): string[] {
-	let res: string[] = [];
-	if (s === "") return res;
+	const res: string[] = [];
 
+	if (s === "") return res;
 	for (let i = 0; i < s.length; i += k) {
 		res.push(s.substring(i, i + k));
 	}
+
 	if (res[res.length - 1].length < k) {
 		let left = k - res[res.length - 1].length;
-		while (left > 0) {
+		while (left) {
 			res[res.length - 1] += fill;
 			left--;
 		}
 	}
-
 	return res;
 }
