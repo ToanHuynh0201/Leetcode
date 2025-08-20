21007ad8b691d7090217952269656e14f44b21ea
ToanHuynh0201 <toanhuynhlilyiu@gmail.com>
2025-08-20 17:39:51 +0700

    160

---
+++ Day21/160-Intersection-of-Two-Linked-Lists.ts
@@ -0,0 +1,16 @@
+function getIntersectionNode(
+	headA: ListNode | null,
+	headB: ListNode | null
+): ListNode | null {
+	if (!headA || !headB) return null;
+
+	let dummy1: ListNode | null = headA;
+	let dummy2: ListNode | null = headB;
+
+	while (dummy1 !== dummy2) {
+		dummy1 = dummy1 ? dummy1.next : headB;
+		dummy2 = dummy2 ? dummy2.next : headA;
+	}
+
+	return dummy1;
+}
