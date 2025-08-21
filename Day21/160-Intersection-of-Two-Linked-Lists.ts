92f3062438afd95515b4ac1cc6adccb1d11d84f6
ToanHuynh0201 <toanhuynhlilyiu@gmail.com>
2025-08-21 12:24:40 +0700

    202, 234

--- Day21/160-Intersection-of-Two-Linked-Lists.ts
+++ Day21/160-Intersection-of-Two-Linked-Lists.ts
@@ -1,3 +1,12 @@
+class ListNode {
+	val: number;
+	next: ListNode | null;
+	constructor(val?: number, next?: ListNode | null) {
+		this.val = val === undefined ? 0 : val;
+		this.next = next === undefined ? null : next;
+	}
+}
+
 function getIntersectionNode(
 	headA: ListNode | null,
 	headB: ListNode | null
