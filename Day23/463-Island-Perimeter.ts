f89c29896ca1c9a7063280586496fe48faf6b61d
ToanHuynh0201 <toanhuynhlilyiu@gmail.com>
2025-08-22 10:43:32 +0700

    463, 566, 766

---
+++ Day23/463-Island-Perimeter.ts
@@ -0,0 +1,18 @@
+function islandPerimeter(grid: number[][]): number {
+	if (grid === null || grid.length === 0 || grid[0].length === 0) return 0;
+	let perimeter = 0;
+
+	for (let i = 0; i < grid.length; i++) {
+		for (let j = 0; j < grid[i].length; j++) {
+			if (grid[i][j] === 1) {
+				perimeter += 4;
+
+				if (i > 0 && grid[i - 1][j] === 1) perimeter -= 2;
+
+				if (j > 0 && grid[i][j - 1] === 1) perimeter -= 2;
+			}
+		}
+	}
+
+	return perimeter;
+}
