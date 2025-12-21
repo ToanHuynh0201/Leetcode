0e7663564a61647f80ea45d0616a9cf3a84cccfc
Huỳnh Nhật Toàn <huynhatoan0201@gmail.com>
2025-12-21 10:48:08 +0700

    feat: Implement searchMatrix and minEatingSpeed functions for binary search algorithms

---
+++ New/BinarySearch/74.ts
@@ -0,0 +1,24 @@
+function searchMatrix(matrix: number[][], target: number): boolean {
+	const m: number = matrix.length;
+	const n: number = matrix[0].length;
+	let left = 0;
+	let right = m * n - 1;
+
+	while (left <= right) {
+		const mid = (right + left) >> 1;
+
+		const x = Math.floor(mid / n);
+		const y = mid % n;
+		const midValue: number = matrix[x][y];
+
+		if (midValue === target) {
+			return true;
+		} else if (midValue < target) {
+			left = mid + 1;
+		} else {
+			right = mid - 1;
+		}
+	}
+
+	return false;
+}
