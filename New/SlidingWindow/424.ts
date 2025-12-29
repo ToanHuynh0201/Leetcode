2d1edcb8a1e06bd6fcbab2753669d56914d6c69c
Huỳnh Nhật Toàn <huynhatoan0201@gmail.com>
2025-12-29 15:44:43 +0700

    feat: Implement checkInclusion function for substring permutation checking; refactor characterReplacement function

--- New/SlidingWindow/424.ts
+++ New/SlidingWindow/424.ts
@@ -19,6 +19,22 @@
 
 // 	return longest;
 // }
+// function characterReplacement(s: string, k: number): number {
+// 	const map = new Map<string, number>();
+// 	let res = 0;
+// 	let l = 0;
+// 	for (let r = 0; r < s.length; r++) {
+// 		map.set(s[r], (map.get(s[r]) || 0) + 1);
+// 		while (r - l + 1 - Math.max(...map.values()) > k) {
+// 			map.set(s[l], map.get(s[l])! - 1);
+// 			l++;
+// 		}
+
+// 		res = Math.max(res, r - l + 1);
+// 	}
+
+// 	return res;
+// }
 function characterReplacement(s: string, k: number): number {
 	const map = new Map<string, number>();
 	let res = 0;
@@ -29,7 +45,6 @@ function characterReplacement(s: string, k: number): number {
 			map.set(s[l], map.get(s[l])! - 1);
 			l++;
 		}
-
 		res = Math.max(res, r - l + 1);
 	}
 
